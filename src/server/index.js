cfb037a6306e27079a8ebf48fcb68f29ac7ef0ae
Thanh Nam Tran <ttran19@umbc.edu>
2022-12-26 08:44:37 -0500

    testing

--- src/server/index.js
+++ src/server/index.js
@@ -26,10 +26,17 @@ app.use((req, res, next) => {
 
 app.use("/post", musicposts);
 app.use("/", users);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
+if (process.env.NODE_ENV === "production") {
+  // Exprees will serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const uri =
   "mongodb+srv://" +
   process.env.dbuser +
