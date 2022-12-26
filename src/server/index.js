da0c880457ea9b9185d1aa860f6fe315d5ecfab1
Thanh Nam Tran <ttran19@umbc.edu>
2022-12-26 08:42:31 -0500

    testing

--- src/server/index.js
+++ src/server/index.js
@@ -26,7 +26,9 @@ app.use((req, res, next) => {
 
 app.use("/post", musicposts);
 app.use("/", users);
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const uri =
   "mongodb+srv://" +
