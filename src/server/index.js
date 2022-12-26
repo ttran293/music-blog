0ea92339b407a19b68577401425c862f4ba6e947
Thanh Nam Tran <ttran19@umbc.edu>
2022-12-26 14:36:05 -0500

    testing deploy

--- src/server/index.js
+++ src/server/index.js
@@ -24,15 +24,13 @@ app.use((req, res, next) => {
   next();
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join("build", "index.html"));
-  });
-}
 
 app.use("/post", musicposts);
 app.use("/", users);
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.resolve("index.html"));
+})
 
 
 const uri =
