a03d607e47da10f175ad5bd88cf5a6b1f084c19b
Thanh Nam Tran <ttran19@umbc.edu>
2022-12-27 02:40:35 -0500

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: "bundle.js",
-    publicPath: "/",
+    //publicPath: "/",
   },
   module: {
     rules: [
