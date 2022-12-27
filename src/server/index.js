17dfd06ab6e68fefd0115e5ab0fd8f13dc3d063d
Thanh Nam Tran <ttran19@umbc.edu>
2022-12-27 02:13:30 -0500

    testing

--- src/server/index.js
+++ src/server/index.js
@@ -28,7 +28,7 @@ app.use("/", users);
 app.use(express.static("/dist"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve("dist", "index.html"));
+  res.sendFile(path.resolve("dist", "bundle.js"));
 });
 
 const uri =
